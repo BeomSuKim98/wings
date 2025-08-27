f97b77a0166fe342aa7d6810a2be244df896a6bd
BeomSuKim98 <qja0427@gmail.com>
2025-08-28 01:23:29 +0900

    초기 설정 끝

---
+++ src/main/java/com/wings/AEI/AeiApplication.java
@@ -0,0 +1,13 @@
+package com.wings.AEI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wings/AEI/AeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wings.AEI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
