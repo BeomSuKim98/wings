77292bc90f756892786d97ac745eed67c79f98f4
BeomSuKim98 <qja0427@gmail.com>
2025-09-01 01:14:19 +0900

    header/footer 디자인 및 JSP -> 타임리프로 변경 scss구조 제작, 파싱

--- src/main/java/com/wings/AEI/controller/PageController.java
+++ src/main/java/com/wings/AEI/controller/PageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping
@@ -13,8 +13,7 @@ public class PageController {
     @GetMapping("/home")
     public String home(Model model){
         model.addAttribute("title","홈");
-        model.addAttribute("body","/WEB-INF/views/home/index.jsp"); // 본문 JSP 논리이름
-        return "common/layout";
+        return "layout";
     }
 
 }
